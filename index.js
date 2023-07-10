9c0b2b93b4251a18127bccbd0b9d0167ea527771
kiyf2004 <jbedi91@gmail.com>
2023-07-10 16:11:19 +0100

    update on websocket

--- index.js
+++ index.js
@@ -1,88 +1,91 @@
+const Web3 = require('web3');
 const Big = require("big.js");
-const blk = require("./blockchain");
+
+const UniswapV3PairABI = require("./abi/IUniswapV3Pair.json");
 const UniswapV2Pair = require("./abi/IUniswapV2Pair.json");
 
-// define address of Pair contract
-const PAIR_ADDR = "0xa478c2975ab1ea89e8196811f51a7b7ade33eb11";
-const PAIR_NAME = "ETH/DAI";
-const INTERVAL = 1000;
-
-// create web3 contract object
-const PairContractHTTP = new blk.web3http.eth.Contract(
-    UniswapV2Pair.abi,
-    PAIR_ADDR
-);
-const PairContractWSS = new blk.web3ws.eth.Contract(
-    UniswapV2Pair.abi,
-    PAIR_ADDR
-);
-
-// reserve state
+// State for V2 reserves
 const state = {
     blockNumber: undefined,
     token0: undefined,
     token1: undefined,
 };
 
-// function to get reserves
-const getReserves = async (ContractObj) => {
-    // call getReserves function of Pair contract
-    const _reserves = await ContractObj.methods.getReserves().call();
-
-    // return data in Big Number
+// function to get reserves for V2 pair
+const getReserves = async (pairContract) => {
+    const _reserves = await pairContract.methods.getReserves().call();
     return [Big(_reserves.reserve0), Big(_reserves.reserve1)];
 };
 
-// sleep function to pause program
-const sleep = (timeInMs) =>
-    new Promise((resolve) => setTimeout(resolve, timeInMs));
-
-const mainHTTP = async () => {
-    // check price continuously
-    while (true) {
-        // get reserves
-        const [amtToken0, amtToken1] = await getReserves(PairContractHTTP);
-
-        // calculate price and print
-        console.log(
-            `Price ${PAIR_NAME} : ${amtToken0.div(amtToken1).toString()}`
-        );
-
-        // wait for some time
-        await sleep(INTERVAL);
-    }
-};
-
 const updateState = (data) => {
-    // update state
     state.token0 = Big(data.returnValues.reserve0);
     state.token1 = Big(data.returnValues.reserve1);
     state.blockNumber = data.blockNumber;
-
-    // calculate price and print
     console.log(
-        `${state.blockNumber} Price ${PAIR_NAME} : ${state.token0
+        `${state.blockNumber} Price: ${state.token0
             .div(state.token1)
             .toString()}`
     );
 };
 
-const mainWSS = async () => {
-    // fetch current state of reserves
-    [state.token0, state.token1] = await getReserves(PairContractHTTP);
-
-    // get current block number
-    state.blockNumber = await blk.web3http.eth.getBlockNumber();
-
-    // subscribe to Sync event of Pair
-    PairContractWSS.events.Sync({}).on("data", (data) => updateState(data));
-
-    // calculate price and print
-    console.log(
-        `${state.blockNumber} Price ${PAIR_NAME} : ${state.token0
-            .div(state.token1)
-            .toString()}`
+const mainFunction = async (version, address, apiKey) => {
+    let provider = new Web3.providers.WebsocketProvider(
+        `wss://mainnet.infura.io/ws/v3/${apiKey}`
     );
+
+    const web3 = new Web3(provider);
+
+    let pairContract;
+    let abi;
+
+    if (version === "v2") {
+        abi = UniswapV2Pair.abi;
+    } else if (version === "v3") {
+        abi = UniswapV3PairABI;
+    } else {
+        console.error("Unsupported Uniswap version");
+        return;
+    }
+
+    pairContract = new web3.eth.Contract(abi, address);
+
+    // For V3 version
+    if (version === "v3") {
+        const slot0 = await pairContract.methods.slot0().call();
+        const currentTick = Number(slot0.tick);
+        const price = Math.pow(1.0001, currentTick);
+        console.log(`Price: ${price}`);
+
+        pairContract.events.Swap()
+            .on('data', (event) => {
+                const blockNumber = event.blockNumber;
+                const tick = Number(event.returnValues.tick);
+                const price = Math.pow(1.0001, tick);
+                console.log(`Price: ${price}`);
+            })
+            .on('error', (error) => {
+                console.error(error);
+                if (error.code === 'ECONNRESET') {
+                    console.log('Connection lost. Reconnecting...');
+                    provider = new Web3.providers.WebsocketProvider(
+                        `wss://mainnet.infura.io/ws/v3/${apiKey}`
+                    );
+                    web3.setProvider(provider);
+                }
+            });
+    } 
+    // For V2 version
+    else if (version === "v2") {
+        [state.token0, state.token1] = await getReserves(pairContract);
+        state.blockNumber = await web3.eth.getBlockNumber();
+        pairContract.events.Sync({}).on("data", (data) => updateState(data));
+        console.log(
+            `Price: ${state.token0
+                .div(state.token1)
+                .toString()}`
+        );
+    }
 };
 
-mainWSS();
+mainFunction("v3", "0x4585FE77225b41b697C938B018E2Ac67Ac5a20c0", "6decd2494d714b53b201a9642db5f1f8"); // V3 BTC/ETH pair
+mainFunction('v2','0xa478c2975ab1ea89e8196811f51a7b7ade33eb11','6decd2494d714b53b201a9642db5f1f8');// V2 DAI/ETH Pair
